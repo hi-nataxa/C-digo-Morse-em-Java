6ef6b8346c1520b91adc7bba1d09b63a6da5bc3a
Priscila Bueno de Souza <161883334+PriscilaB701@users.noreply.github.com>
2025-10-03 13:32:53 -0300

    insert atualizado

--- src/main/java/codigoMorse/MorseBST.java
+++ src/main/java/codigoMorse/MorseBST.java
@@ -4,7 +4,6 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
 
-
 public class MorseBST {
     private Node root;
 
@@ -16,26 +15,26 @@ public class MorseBST {
         return root == null;
     }
 
-    public void insert(char letter, String code) {
-        Node current = root;
+    public void insertPublic(char letter, String morseCode) {
+        root = insert(root, letter, morseCode, 0);
+    }
 
-        for (int i = 0; i < code.length(); i++) {
-            char character = code.charAt(i);
-
-            if (character == '.') {
-                if (current.left == null) {
-                    current.left = new Node('*');
-                }
-                current = current.left;
-            } else if (character == '-') {
-                if (current.right == null) {
-                    current.right = new Node('*');
-                }
-                current = current.right;
-            }
+    private Node insert(Node node, char letter, String code, int index) {
+        if (index == code.length()) {
+            return new Node(letter);
         }
 
-        current.letter = letter;
+        if (node == null) {
+            node = new Node(' ');
+        }
+
+        char symbol = code.charAt(index);
+        if (symbol == '.') {
+            node.left = insert(node.left, letter, code, index + 1);
+        } else if (symbol == '-') {
+            node.right = insert(node.right, letter, code, index + 1);
+        }
+        return node;
     }
 
     private String findCode(Node node, char letter, String code) {
@@ -73,9 +72,49 @@ public class MorseBST {
         return code + encodeWord(word.substring(1));
     }
 
-    public String decodeWord(String morse) {
+    public void checkString(String word){
+        word = word.toUpperCase().trim();
+        if (word.isEmpty()) return;
 
-        return "[Decodificação não implementada]";
+        char c = word.charAt(0);
+        if (!Character.isLetter(c) && c != ' '){
+            throw new IllegalArgumentException("Caractere inválido '" + c +
+                    "'. Somente letras e espaços são permitidos, tente novamente");
+        }
+        checkString(word.substring(1));
+    }
+
+    private String decodeWord(String morse, Node root) {
+
+        morse = morse.trim();
+        if (morse.isEmpty()) return "";
+        int espaço = morse.indexOf(' ');
+        String letraCode;
+        String resto;
+        if (espaço == -1){
+            letraCode = morse;
+            resto = "";
+        } else {
+            letraCode = morse.substring(0, espaço);
+            resto = morse.substring(espaço + 1);
+        }
+        char letra = decodeLetter(letraCode, root, 0);
+        return letra + decodeWord(resto, root);
+    }
+
+    private char decodeLetter(String code, Node atual, int index){
+        if (atual == null) throw new IllegalArgumentException("Código Morse Inválido " +
+                "ou ainda não inserido, insira e tente novamente");
+        if (index == code.length()) return code.equals("/") ? ' ': atual.letter;
+
+        char c = code.charAt(index);
+        if (c == '.') return decodeLetter(code, atual.left, index + 1);
+        if (c == '-') return decodeLetter(code, atual.right, index +1);
+        if (c == '/') return ' ';
+        throw new IllegalArgumentException("O caracter '" + c + "' é inválido para código morse.");
+    }
+    public String decodeWord(String morse){
+        return decodeWord(morse, this.root);
     }
 
     public int getHeight() {
